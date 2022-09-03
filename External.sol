859e53be00e1dfccf8c7a4343ade7e4f42570b92
nftmerchant <nftmerchant@gmail.com>
2022-09-03 11:28:29 -0700

    Library.sol

---
+++ External.sol
@@ -0,0 +1,6 @@
+// https://github.com/owner/repo/blob/branch/path/to/Contract.sol
+import "https://github.com/owner/repo/blob/branch/path/to/Contract.sol";
+
+// Example import ECDSA.sol from openzeppelin-contract repo, release-v4.5 branch
+// https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.5/contracts/utils/cryptography/ECDSA.sol
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.5/contracts/utils/cryptography/ECDSA.sol";
